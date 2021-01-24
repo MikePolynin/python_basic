49bbe2f2d285290068adea4bbe1b8d363a639960
mike_p <mike.polynin@gmail.com>
2021-01-24 23:14:20 +0300

    Task 09 solved

--- Module16/09_friends/main.py
+++ Module16/09_friends/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+def friends():
+    n = int(input('Кол-во друзей: '))
+    k = int(input('Долговых расписок: '))
+
+    count = 0
+    result = [0] * n
+
+    while count < k:
+        print()
+        print(count + 1, 'расписка')
+        money_to = int(input('Кому: '))
+        money_from = int(input('От кого: '))
+        money_sum = int(input('Сколько: '))
+        result[money_to - 1] -= money_sum
+        result[money_from - 1] += money_sum
+        count += 1
+
+    print('\nБаланс друзей:')
+    for i in range(n):
+        print(i + 1, ':', result[i])
+
+
+friends()
