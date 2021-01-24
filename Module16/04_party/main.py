3ff0ff1c12e2541558f80c5473c10fd0c08dba52
mike_p <mike.polynin@gmail.com>
2021-01-24 19:24:52 +0300

    Task 04 solved

--- Module16/04_party/main.py
+++ Module16/04_party/main.py
@@ -1,3 +1,29 @@
-guests = ['Петя', 'Ваня', 'Саша', 'Лиза', 'Катя']
+def party():
+    guests = ['Петя', 'Ваня', 'Саша', 'Лиза', 'Катя']
 
-# TODO здесь писать код
+    while True:
+        print('\nСейчас на вечеринке', len(guests), 'человек:', guests)
+        user_input = input('Гость пришел или ушел? ')
+
+        if user_input == 'пришел' or user_input == 'Пришел':
+            name = input('Имя гостя: ')
+            if len(guests) < 6:
+                print('Привет, ' + name)
+                guests.append(name)
+            else:
+                print('Прости, ' + name + ', но мест нет.')
+
+        elif user_input == 'ушел' or user_input == 'Ушел':
+            name = input('Имя гостя: ')
+            print('Пока, ' + name + '!')
+            guests.remove(name)
+
+        elif user_input == 'пора спать' or user_input == 'Пора спать':
+            print('\nВечеринка закончилась, все легли спать.')
+            break
+
+        else:
+            print('Некорректный ввод')
+
+
+party()
