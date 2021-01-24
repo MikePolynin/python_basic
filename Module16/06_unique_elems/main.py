5b03918386c6e14899dc04a8a2975fb1fa2e8b0c
mike_p <mike.polynin@gmail.com>
2021-01-24 20:53:41 +0300

    Task 06 solved

--- Module16/06_unique_elems/main.py
+++ Module16/06_unique_elems/main.py
@@ -1,1 +1,24 @@
-# TODO здесь писать код
+def unique_elements():
+    list_1 = []
+    list_2 = []
+
+    print('Заполнение 1 списка:')
+    for _ in range(3):
+        digit = int(input('Введите число: '))
+        list_1.append(digit)
+
+    print('\nЗаполнение 2 списка:')
+    for _ in range(7):
+        digit = int(input('Введите число: '))
+        list_2.append(digit)
+
+    print('\nПервый список:', list_1)
+    print('Второй список:', list_2)
+
+    list_1.extend(list_2)
+    list_1 = set(list_1)
+
+    print('\nНовый первый список с уникальными элементами:', list_1)
+
+
+unique_elements()
