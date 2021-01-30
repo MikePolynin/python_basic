d009c851bc6f62bd3a8d38b5c9aeaef4fe00b8e1
mike_p <mike.polynin@gmail.com>
2021-01-30 23:47:16 +0300

    17.02

--- Module17/02_generation/main.py
+++ Module17/02_generation/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+def generation():
+    length = int(input('Введите длину списка: '))
+
+    result = [1 if i % 2 == 0 else i % 5 for i in range(length)]
+
+    print('Результат:', result)
+
+
+generation()
