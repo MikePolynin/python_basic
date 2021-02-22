9d1bfef7739a823ac5d428ed17d33587704925a2
mike_p <mike.polynin@gmail.com>
2021-02-22 11:57:41 +0300

    18.04

--- Module18/04_capital_letters/main.py
+++ Module18/04_capital_letters/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+def capital_letters():
+    user_input = input('Введите строку: ')
+
+    output_string = ' '.join(word[0].upper() + word[1:] for word in user_input.split())
+
+    print('Результат: ' + output_string)
+
+
+capital_letters()
