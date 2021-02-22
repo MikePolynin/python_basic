ab3d745c1f1c71efeccd0696bf5338ab35327fdc
mike_p <mike.polynin@gmail.com>
2021-02-22 11:43:28 +0300

    18.03

--- Module18/03_files/main.py
+++ Module18/03_files/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+def files():
+    filename = input('Название файла: ')
+
+    spec_symbols = ('@', '№', '\u005C', '$', '%', '^', '&', '*', '(', ')')
+    ends = ('.txt', '.docx')
+
+    if filename.startswith(spec_symbols):
+        print('\nОшибка: название начинается на один из специальных символов')
+    elif not filename.endswith(ends):
+        print('\nОшибка: неверное расширение файла. Ожидалось .txt или .docx')
+    else:
+        print('\nФайл назван верно.')
+
+
+files()
