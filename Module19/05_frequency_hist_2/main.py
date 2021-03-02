c4f5d249e35623f53d668271e153cc6dde6c35ca
mike_p <mike.polynin@gmail.com>
2021-03-03 00:09:50 +0300

    19.05

--- Module19/05_frequency_hist_2/main.py
+++ Module19/05_frequency_hist_2/main.py
@@ -1,1 +1,33 @@
-# TODO здесь писать код
+def frequency_hist_2():
+    input_string = input('Введите текст: ')
+
+    letter_dict = dict()
+    unique_letters = []
+    letter_list = []
+
+    for symbol in input_string:
+        if symbol in letter_dict:
+            letter_dict[symbol] = letter_dict.pop(symbol) + 1
+        else:
+            letter_dict[symbol] = 1
+
+    print('Оригинальный словарь частот:')
+
+    for key in letter_dict:
+        print(key, ':', letter_dict[key])
+
+    for value in letter_dict.values():
+        if value not in unique_letters:
+            unique_letters.append(value)
+
+    print('\nИнвертированный словарь частот:')
+
+    for value in unique_letters:
+        letter_list.clear()
+        for key in letter_dict.keys():
+            if letter_dict[key] == value:
+                letter_list.append(key)
+        print(value, ':', letter_list)
+
+
+frequency_hist_2()
