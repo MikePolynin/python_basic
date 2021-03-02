4f0e9cd381600dc50a51607d19fb3933e2df3ec9
mike_p <mike.polynin@gmail.com>
2021-03-03 00:49:40 +0300

    19.06

--- Module19/06_synonym_dict/main.py
+++ Module19/06_synonym_dict/main.py
@@ -1,1 +1,31 @@
-# TODO здесь писать код
+def synonym(syn_dict):
+    user_word = input('Введите слово: ')
+
+    for key in syn_dict:
+        if key.lower() == user_word.lower():
+            print('Синоним:', syn_dict[key])
+        else:
+            print('Такого слова в словаре нет.')
+            synonym(syn_dict)
+        break
+
+
+def synonym_dict():
+    syn_quantity = int(input('Введите количество пар слов: '))
+
+    syn_dict = dict()
+
+    for pair in range(syn_quantity):
+        pair_str = input('{0} пара: '.format(pair + 1))
+        word_list = pair_str.replace(' ', '').split('-')
+
+        syn_dict[word_list[0]] = word_list[1]
+        syn_dict[word_list[1]] = word_list[0]
+
+        word_list.clear()
+
+    print()
+    synonym(syn_dict)
+
+
+synonym_dict()
