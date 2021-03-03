cd1324e48a778fb0b85c28a9e63d5c6d166a94af
mike_p <mike.polynin@gmail.com>
2021-03-03 21:42:02 +0300

    19.09

--- Module19/09_pedigree/main.py
+++ Module19/09_pedigree/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+def pedigree():
+    quantity = int(input('Введите количество человек: '))
+
+    tree = dict()
+
+    for i in range(quantity):
+        pair = input('{0} пара: '.format(i + 1))
+
+        pair_list = pair.split(' ')
+
+        if i == 0:
+            tree[pair_list[1]] = 0
+
+        parent_number = tree[pair_list[1]]
+        tree[pair_list[0]] = parent_number + 1
+
+    print('“\nВысота” каждого члена семьи:')
+    for key in tree:
+        print(key, tree[key])
+
+
+pedigree()
