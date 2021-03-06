3cd043a061d3b710ac8d31d70d6a7d65210408ce
mike_p <mike.polynin@gmail.com>
2021-03-06 16:23:38 +0300

    20.03

--- Module20/03_function/main.py
+++ Module20/03_function/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+def function(user_tuple, user_symbol):
+    result_tuple = tuple()
+
+    if user_symbol in user_tuple:
+        count = 0
+
+        for symbol in user_tuple:
+            if symbol == user_symbol:
+                count += 1
+            if count == 2:
+                break
+
+        first_index = user_tuple.index(user_symbol)
+
+        if count == 1:
+            last_index = len(user_tuple) - 1
+        else:
+            last_index = user_tuple.index(user_symbol, first_index + 1, len(user_tuple))
+
+        result_tuple = user_tuple[first_index: last_index + 1]
+
+    return result_tuple
