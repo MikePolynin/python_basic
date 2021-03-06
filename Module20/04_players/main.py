dc5e1a1145c50c0a1d1fc8ba7d13b60da174b1c8
mike_p <mike.polynin@gmail.com>
2021-03-06 16:30:38 +0300

    20.04

--- Module20/04_players/main.py
+++ Module20/04_players/main.py
@@ -1,7 +1,13 @@
-players = {
-    ("Ivan", "Volkin"): (10, 5, 13),
-    ("Bob", "Robbin"): (7, 5, 14),
-    ("Rob", "Bobbin"): (12, 8, 2)
-}
+def players_func():
+    players = {
+        ("Ivan", "Volkin"): (10, 5, 13),
+        ("Bob", "Robbin"): (7, 5, 14),
+        ("Rob", "Bobbin"): (12, 8, 2)
+    }
 
-# TODO здесь писать код
+    new_players = [(player_name + player_score) for player_name, player_score in players.items()]
+
+    print(new_players)
+
+
+players_func()
