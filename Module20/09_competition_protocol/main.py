402177b592c01b0eac508c84b34af22e8b51ecf5
mike_p <mike.polynin@gmail.com>
2021-03-07 16:53:48 +0300

    20.09

--- Module20/09_competition_protocol/main.py
+++ Module20/09_competition_protocol/main.py
@@ -1,1 +1,31 @@
-# TODO здесь писать код
+def competition_protocol():
+    result_dict = dict()
+
+    quantity = int(input('Сколько записей вносится в протокол? '))
+
+    print('Записи (результат и имя): ')
+    for i in range(quantity):
+        result = input('{0} запись: '.format(i + 1))
+
+        result_list = result.split(' ')
+
+        if result_list[1] not in result_dict:
+            result_dict[result_list[1]] = int(result_list[0])
+        elif int(result_list[0]) > result_dict[result_list[1]]:
+            result_dict[result_list[1]] = int(result_list[0])
+
+    print('\nИтоги соревнований: ')
+    for i in range(3):
+        result = max(result_dict.values())
+
+        for player in result_dict:
+            if result_dict[player] == result:
+                print('{0} место: {1} ({2})'.format(
+                    i + 1,
+                    player,
+                    result_dict[player]
+                ))
+                result_dict[player] = 0
+
+
+competition_protocol()
