14cd9e546c8896a37224120ad8dfd23d4d033e20
mike_p <mike.polynin@gmail.com>
2021-03-07 21:09:39 +0300

    20.03

--- Module20/03_function/main.py
+++ Module20/03_function/main.py
@@ -2,21 +2,15 @@ def function(user_tuple, user_symbol):
     result_tuple = tuple()
 
     if user_symbol in user_tuple:
-        count = 0
-
-        for symbol in user_tuple:
-            if symbol == user_symbol:
-                count += 1
-            if count == 2:
-                break
-
         first_index = user_tuple.index(user_symbol)
+        last_index = first_index
 
-        if count == 1:
-            last_index = len(user_tuple) - 1
-        else:
-            last_index = user_tuple.index(user_symbol, first_index + 1, len(user_tuple))
+        if user_symbol in user_tuple[first_index + 1:]:
+            last_index = user_tuple.index(user_symbol, first_index + 1)
 
         result_tuple = user_tuple[first_index: last_index + 1]
 
     return result_tuple
+
+
+function((2, 3, 4, 2, 5, 6), 2)
