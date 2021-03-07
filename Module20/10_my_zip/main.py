e265e79db2fd8b84ab4a326d767cd431f13f9736
mike_p <mike.polynin@gmail.com>
2021-03-07 20:43:50 +0300

    20.10

--- Module20/10_my_zip/main.py
+++ Module20/10_my_zip/main.py
@@ -1,28 +1,18 @@
 def make_zip(a, b):
-    result_list = []
-
-    if len(a) > len(b):
-        result_len = len(b)
-    else:
-        result_len = len(a)
-
-    for i in range(result_len):
-        pair = (a[i], b[i])
-        result_list.append(pair)
-
-    return result_list
+    return [(a[i], b[i]) for i in range(min(len(a), len(b)))]
 
 
 def my_zip():
     input_string = input('Строка: ')
-    input_tuple = input('Кортеж чисел: ')
+    input_tuple = tuple(input('Кортеж чисел: ').split(', '))
+
+    print(input_tuple)
+    print(input_tuple[0])
 
     # pairs = zip(input_string, input_tuple)
     pairs = make_zip(input_string, input_tuple)
 
     print('\nРезультат:')
-    print(pairs)
-
     for pair in pairs:
         print(pair)
 
