08b773b73f6399e7f3a0d5c6e7aa4e7ff216d7b5
mike_p <mike.polynin@gmail.com>
2021-03-13 22:59:45 +0300

    21.03

--- Module21/03_fibonacci/main.py
+++ Module21/03_fibonacci/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+def get_fib_number(num_pos, prev_number=1, number=1, number_index=0):
+    if number_index == int(num_pos):
+        print('\nЧисло:', number)
+        return number
+    get_fib_number(num_pos=num_pos, prev_number=number, number=prev_number + number, number_index=number_index + 1)
+
+
+def fibonacci():
+    num_pos = input('Введите позицию числа в ряде Фибоначчи: ')
+
+    get_fib_number(num_pos)
+
+
+fibonacci()
