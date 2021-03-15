54c7d40214c59b0b20dc8b88703163f506f17940
mike_p <mike.polynin@gmail.com>
2021-03-16 00:11:15 +0300

    21.05

--- Module21/05_make_function_faster/main.py
+++ Module21/05_make_function_faster/main.py
@@ -1,9 +1,27 @@
+nums = dict()
+
+
 def calculating_math_func(data):
-    result = 1
-    for index in range(1, data + 1):
-        result *= index
+    global nums
+
+    if data in nums:
+        result = nums[data]
+    else:
+        result = 1
+        for index in range(1, data + 1):
+            result *= index
+        nums[data] = result
+
     result /= data ** 3
     result = result ** 10
     return result
 
-# TODO оптимизировать функцию
+
+def make_function_faster():
+    while True:
+        number = int(input('Введите число: '))
+
+        print(calculating_math_func(number))
+
+
+make_function_faster()
