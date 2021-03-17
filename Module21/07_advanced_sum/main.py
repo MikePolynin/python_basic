4fb8adc4f84d82e504db286165ed663b9bd35fbc
mike_p <mike.polynin@gmail.com>
2021-03-17 22:15:14 +0300

    21.07

--- Module21/07_advanced_sum/main.py
+++ Module21/07_advanced_sum/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+def sum_count(numbers, result=0):
+    for i in range(len(numbers)):
+        if isinstance(numbers[i], int):
+            result += numbers[i]
+        else:
+            result += sum_count(numbers[i], 0)
+
+    return result
+
+
+def advanced_sum(numbers):
+    if isinstance(numbers, int):
+        print(numbers)
+    else:
+        print('Ответ:', sum_count(numbers))
+
+
+advanced_sum([6, [3, 2], 2])
