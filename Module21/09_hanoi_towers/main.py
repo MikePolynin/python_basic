612a2acd65f1ebde02be9d690cb8db0c32c270d9
mike_p <mike.polynin@gmail.com>
2021-03-18 00:12:19 +0300

    21.09

--- Module21/09_hanoi_towers/main.py
+++ Module21/09_hanoi_towers/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+def move(quantity, from_where, where):
+    middle = 6 - from_where - where
+
+    if quantity >= 1:
+        move(quantity - 1, from_where, middle)
+
+        print('Переложить диск {0} со стержня номер {1} на стержень номер {2}'.format(
+            quantity,
+            from_where,
+            where))
+
+        move(quantity - 1, middle, where)
+
+
+def hanoi_towers():
+    quantity = int(input('Введите количество дисков: '))
+
+    move(quantity, 1, 2)
+
+
+hanoi_towers()
