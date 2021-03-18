8beae22a69e2547d7427ff5061533539ebefaaa6
mike_p <mike.polynin@gmail.com>
2021-03-18 20:43:08 +0300

    21.02

--- Module21/02_my_zip_2/main.py
+++ Module21/02_my_zip_2/main.py
@@ -7,16 +7,15 @@ def make_zip(a, b, index=0, result=None):
 
     pair = (a[index], b[index])
     result.append(pair)
-    print(pair)
-    make_zip(a, b, index + 1, result)
+
+    return make_zip(a, b, index + 1, result)
 
 
 def my_zip_2():
     input_string = input('Первый набор символов через запятую с пробелом: ').split(', ')
     input_tuple = input('Второй набор символов через запятую с пробелом: ').split(', ')
 
-    print('\nРезультат:')
-    make_zip(input_string, input_tuple)
+    return make_zip(input_string, input_tuple)
 
 
-my_zip_2()
+print('\nРезультат:', my_zip_2())
