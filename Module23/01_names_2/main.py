d4f43a3d1fa89f8c615d71a59900a56242238105
mike_p <mike.polynin@gmail.com>
2021-04-03 14:29:15 +0300

    23.01

--- Module23/01_names_2/main.py
+++ Module23/01_names_2/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+def names_2():
+    total_length = 0
+    line_count = 0
+    try:
+        with open('people.txt', 'r', encoding='utf-8') as people:
+            for line in people:
+                line_count += 1
+                length = len(line)
+                if line.endswith('\n'):
+                    length -= 1
+                if length < 3:
+                    print('Строка {} слишком коротка'.format(line_count))
+                else:
+                    total_length += length
+    finally:
+        print('Сумма символов в строках с длиной больше 3: {0}'.format(total_length))
+
+
+names_2()
