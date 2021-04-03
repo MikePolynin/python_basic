b83460195c5883dcdf8ab7b0028afbdbc06314e6
mike_p <mike.polynin@gmail.com>
2021-04-03 19:01:30 +0300

    23.03

--- Module23/03_lucky_number/main.py
+++ Module23/03_lucky_number/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+def lucky_number():
+    total_sum = 0
+    numbers = list()
+    while total_sum < 777:
+        try:
+            number = input('Введите число: ')
+            if number.isalpha():
+                raise TypeError
+            total_sum += int(number)
+            numbers.append(number + '\n')
+        except TypeError:
+            print('Некорректный ввод')
+        finally:
+            with open('results.txt', 'w') as results:
+                for num in numbers:
+                    results.write(num)
+
+
+lucky_number()
