767923c3460c60202f0acd79843ad7895a7d511b
mike_p <mike.polynin@gmail.com>
2021-04-05 21:46:11 +0300

    23.03

--- Module23/03_lucky_number/main.py
+++ Module23/03_lucky_number/main.py
@@ -3,25 +3,21 @@ import random
 
 def lucky_number():
     total_sum = 0
-    numbers = list()
-    while total_sum < 777:
-        try:
-            number = input('Введите число: ')
-            if number.isalpha():
-                raise TypeError
-            total_sum += int(number)
-            numbers.append(number + '\n')
-        except TypeError:
-            print('Некорректный ввод')
-        finally:
-            chance = random.randint(0, 13)
-            if chance == 13:
-                raise BaseException('Ошибка, данные не будут записаны')
-            else:
-                with open('results.txt', 'w') as results:
-                    for num in numbers:
-                        results.write(num)
+    with open('results.txt', 'w') as results:
+        while total_sum < 777:
+            try:
+                number = input('Введите число: ')
+                if not isinstance(number, int):
+                    raise TypeError
+                total_sum += int(number)
+            except TypeError:
+                print('Некорректный ввод')
+            finally:
+                chance = random.randint(0, 13)
+                if chance == 13:
+                    raise BaseException('Ошибка, данные не будут записаны')
+                else:
+                    results.write(number + '\n')
 
 
 lucky_number()
-
