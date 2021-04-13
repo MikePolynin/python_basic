fb4935d853a13a5d6b96649aa00f3dff78d76150
mike_p <mike.polynin@gmail.com>
2021-04-13 20:53:09 +0300

    24.02

--- Module24/02_students/main.py
+++ Module24/02_students/main.py
@@ -1,1 +1,24 @@
-# TODO здесь писать код
+from student import Student
+import random
+
+
+def students():
+    names = ['Ivan', 'Petr', 'Andrey', 'Vasiliy', 'Pavel', 'Oleg', 'Ilya', 'Kirill', 'Dmitriy', 'Aleksey']
+    ave_score_dict = dict()
+
+    students_list = [Student(names[i], random.randint(1, 10), [random.randint(1, 5) for _ in range(5)])
+                     for i in range(10)]
+
+    for student in students_list:
+        ave_score = 0
+        for score in student.progress:
+            ave_score += score
+        ave_score_dict[student] = round(ave_score / 5, 2)
+
+    for ave_score in sorted(set(ave_score_dict.values())):
+        for student, score in ave_score_dict.items():
+            if ave_score == score:
+                print('Средний балл: {}, ФИ студента: {}'.format(score, student.name))
+
+
+students()
