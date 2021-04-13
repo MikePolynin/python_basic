1b40f56b465ad235824b2dfe7d109e1010b6a2bc
mike_p <mike.polynin@gmail.com>
2021-04-14 00:14:11 +0300

    24.04

---
+++ Module24/04_fathers_mothers_kids/kid.py
@@ -0,0 +1,6 @@
+class Kid:
+    def __init__(self, name='', age=0, is_calm=True, is_hungry=False):
+        self.name = name
+        self.age = age
+        self.is_calm = is_calm
+        self.is_hungry = is_hungry
--- Module24/04_fathers_mothers_kids/main.py
+++ Module24/04_fathers_mothers_kids/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+from kid import Kid
+from parent import Parent
+
+
+def father_mother_kid():
+    kid_1 = Kid('Vasya', 5, True, False)
+    kid_2 = Kid('Petya', 10, True, False)
+    kid_3 = Kid('Ilya', 30, True, False)
+    parent = Parent('Boris', 35, [kid_1, kid_2, kid_3])
+
+    print(parent.kids[0].age)
+
+
+father_mother_kid()
