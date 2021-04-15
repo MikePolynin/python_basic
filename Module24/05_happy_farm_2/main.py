a065a75e02d29c85da65baa1e917d63e4ef5950c
mike_p <mike.polynin@gmail.com>
2021-04-15 22:34:41 +0300

    24.05

--- Module24/05_happy_farm_2/main.py
+++ Module24/05_happy_farm_2/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+from potatogarden import PotatoGarden
+from gardener import Gardener
+
+
+def happy_farm_2():
+    my_garden = PotatoGarden(5)
+    gardener = Gardener('Bob', my_garden)
+    gardener.print_potato_list()
+    gardener.cearing()
+    gardener.cearing()
+    gardener.cearing()
+    gardener.get_potato()
+    gardener.potatogarden.are_all_ripe()
+    gardener.cearing()
+
+
+happy_farm_2()
