02728a5c7653692a23d546ce2ede55e9e2a442b2
mike_p <mike.polynin@gmail.com>
2021-05-08 19:18:55 +0300

    26.05

--- Module26/05_str_count/main.py
+++ Module26/05_str_count/main.py
@@ -1,35 +1,25 @@
 import os
 
-address_list = []
-count = 0
 
-
-def address_collector(address: str) -> None:
-    global count
-    norm_address = os.path.normpath(address)
-    for elem in os.listdir(norm_address):
-        if not os.path.isdir(os.path.join(norm_address, elem)) and elem.endswith('.py'):
-            address_list.append(os.path.join(norm_address, elem))
-        elif os.path.isdir(os.path.join(norm_address, elem)):
-            directory = os.path.join(norm_address, elem)
-            address_collector(directory)
-
-
-def counter(addr_list: list) -> int:
-    global count
-    for addr in addr_list:
-        with open(addr, 'r') as file:
-            for line in file:
-                if line != len(line) * line[0]:
-                    if not line.startswith('#'):
-                        count += 1
-
+def file_line_count(address, count):
+    with open(address, 'r') as file:
+        for line in file:
+            if line != len(line) * line[0]:
+                if not line.startswith('#'):
+                    count += 1
     return count
 
 
-def str_count(address: str) -> int:
-    address_collector(address)
-    return counter(address_list)
+def str_count(address: str, count=0):
+    norm_address = os.path.normpath(address)
+    for elem in os.listdir(norm_address):
+        if not os.path.isdir(os.path.join(norm_address, elem)) and elem.endswith('.py'):
+            file_address = os.path.join(norm_address, elem)
+            count = file_line_count(file_address, count)
+        elif os.path.isdir(os.path.join(norm_address, elem)):
+            directory = os.path.join(norm_address, elem)
+            count = str_count(directory, count)
+    return count
 
 
 print(str_count('F:\\SkillBox\\python_basic\\Module26'))
