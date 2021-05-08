7d917aba94762d15da63adbd94eb1b146085fe66
mike_p <mike.polynin@gmail.com>
2021-05-08 19:57:11 +0300

    26.03

--- Module26/03_files_path/main.py
+++ Module26/03_files_path/main.py
@@ -1,19 +1,18 @@
 import os
 
 
-def gen_files_path(looking_dir: str, address: str = os.path.abspath(os.sep)) -> None:
+def gen_files_path(looking_dir: str, address: str = os.path.abspath(os.sep)):
+    for elem in os.listdir(address):
+        yield os.path.join(address, elem)
+
     for elem in os.listdir(address):
         if os.path.isdir(os.path.join(address, elem)):
             if elem == looking_dir:
                 print('Find')
             else:
-                directory = os.path.join(address, elem)
-                all_file_path = (os.path.join(directory, file) for file in os.listdir(directory))
-
-                for file_path in all_file_path:
-                    print(file_path)
-
-                gen_files_path(looking_dir, os.path.join(address, elem))
+                for element in gen_files_path(looking_dir, os.path.normpath(os.path.join(address, elem))):
+                    yield element
 
 
-gen_files_path('asd', 'D:\\SkillBox\\python_basic\\Module26\\03_files_path')
+for _ in gen_files_path('asd', 'F:\\SkillBox\\python_basic\\Module26\\03_files_path'):
+    print(_)
