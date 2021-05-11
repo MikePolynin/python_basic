2aed428fc286c4dab37dec79a244a1ae64becc02
mike_p <mike.polynin@gmail.com>
2021-05-11 23:22:00 +0300

    27.01

--- Module27/01_how_are_you/main.py
+++ Module27/01_how_are_you/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+from typing import Any, Callable
+import functools
+
+
+def how_are_you(func: Callable) -> Callable:
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs) -> Any:
+        input('Как дела? ')
+        print('А у меня не очень! Ладно, держи свою функцию.')
+
+        result = func(*args, **kwargs)
+
+        return result
+
+    return wrapper()
+
+
+@how_are_you
+def test() -> None:
+    print('Работа основной функции')
+
