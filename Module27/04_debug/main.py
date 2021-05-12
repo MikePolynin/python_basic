6760add6a9fe5350cf449443bc342978878de6b7
mike_p <mike.polynin@gmail.com>
2021-05-12 21:22:45 +0300

    27.04

--- Module27/04_debug/main.py
+++ Module27/04_debug/main.py
@@ -1,1 +1,36 @@
-# TODO здесь писать код
+from typing import Any, Callable
+import functools
+
+
+def debug(func: Callable) -> Callable:
+    """
+    Декоратор, выводящий название функции, принимаемые параметры и результат выполнения.
+    :param func:
+    :return:
+    """
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs) -> Any:
+        print('\nВызывается {}'.format(func.__name__), args, kwargs)
+
+        result = func(*args, **kwargs)
+
+        print("'{0}' вернула значение '{1}'".format(
+            func.__name__,
+            result))
+        print(result)
+
+    return wrapper
+
+
+@debug
+def greeting(name, age=None):
+    if age:
+        return "Ого, {name}! Тебе уже {age} лет, ты быстро растешь!".format(name=name, age=age)
+    else:
+        return "Привет, {name}!".format(name=name)
+
+
+greeting("Том")
+greeting("Миша", age=100)
+greeting(name="Катя", age=16)
