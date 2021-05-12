64f5e09e8fb295a033504c89a74ef1d1119f61bc
mike_p <mike.polynin@gmail.com>
2021-05-12 21:53:48 +0300

    27.05

--- Module27/05_str_count/main.py
+++ Module27/05_str_count/main.py
@@ -1,1 +1,36 @@
-# TODO здесь писать код
+from typing import Any, Callable
+import functools
+
+
+def counter(func: Callable) -> Callable:
+    """
+    Декоратор, считающий количество вызовов функции.
+    :param func:
+    :return:
+    """
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs) -> Any:
+        wrapper.count += 1
+        return func(*args, **kwargs)
+
+    wrapper.count = 0
+    return wrapper
+
+
+@counter
+def greeting(name: str, age: int = None) -> str:
+    if age:
+        return "Ого, {name}! Тебе уже {age} лет, ты быстро растешь!".format(name=name, age=age)
+    else:
+        return "Привет, {name}!".format(name=name)
+
+
+greeting("Том")
+print('Вызов №{}'.format(greeting.count))
+
+greeting("Миша", age=100)
+print('Вызов №{}'.format(greeting.count))
+
+greeting(name="Катя", age=16)
+print('Вызов №{}'.format(greeting.count))
