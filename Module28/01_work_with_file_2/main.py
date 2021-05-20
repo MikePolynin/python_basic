8de714a1a01cdf3ecbd5f5b9838ac89a4fc3d832
mike_p <mike.polynin@gmail.com>
2021-05-20 20:40:19 +0300

    28.01

--- Module28/01_work_with_file_2/main.py
+++ Module28/01_work_with_file_2/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+from typing import TextIO
+
+
+class FileManager:
+    def __init__(self, file_name: str, file_method: str) -> None:
+        self.file_name = file_name
+        self.method = file_method
+
+    def __enter__(self) -> TextIO:
+        try:
+            self.file = open(self.file_name, self.method)
+        except FileNotFoundError:
+            self.file = open(self.file_name, 'w')
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.file.close()
+        return True
