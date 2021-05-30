d3d062e6faa67e1b6667354b21e48bbb8882f49a
mike_p <mike.polynin@gmail.com>
2021-05-30 22:13:40 +0300

    29.02

--- Module29/02_callback/main.py
+++ Module29/02_callback/main.py
@@ -1,1 +1,33 @@
-# TODO здесь писать код
+import functools
+from typing import Callable, Any
+
+app = {}
+
+
+def callback(route: Any) -> Callable:
+    def decorator(func: Callable) -> Callable:
+        app[route] = func
+
+        @functools.wraps(func)
+        def wrapped_func():
+            result = func()
+            return result
+
+        return wrapped_func
+
+    return decorator
+
+
+@callback('//')
+def example():
+    print('Пример функции, которая возвращает ответ сервера')
+    return 'OK'
+
+
+route = app.get('//')
+
+if route:
+    response = route()
+    print('Ответ:', response)
+else:
+    print('Такого пути нет')
