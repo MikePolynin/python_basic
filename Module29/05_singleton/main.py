70a530631544502d9aaf8c12c676b8988d5de82c
mike_p <mike.polynin@gmail.com>
2021-06-05 15:18:44 +0300

    29.05

--- Module29/05_singleton/main.py
+++ Module29/05_singleton/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+from collections.abc import Callable
+
+
+def singleton(cls) -> Callable:
+    def wrap(*args, **kwargs):
+        if not wrap.instance:
+            wrap.instance = cls(*args, **kwargs)
+        return wrap.instance
+
+    wrap.instance = None
+    return wrap
+
+
+@singleton
+class Example:
+    pass
+
+
+my_obj = Example()
+my_another_obj = Example()
+
+print(id(my_obj))
+print(id(my_another_obj))
+
+print(my_obj is my_another_obj)
