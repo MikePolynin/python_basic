957ab84edcd689313daded85e5192d0901e23b58
mike_p <mike.polynin@gmail.com>
2021-06-06 16:12:43 +0300

    29.04

--- Module29/04_decorated_world/main.py
+++ Module29/04_decorated_world/main.py
@@ -2,16 +2,16 @@ from collections.abc import Callable
 
 
 def decorator_with_args_for_any_decorator(decorator: Callable) -> Callable:
-    def decorator_wrap(*args, **kwargs):
+    def decorator_wrap(func, *args, **kwargs):
         print('Переданные арги и кварги в декоратор: {} {}'.format(args, kwargs))
-        result = decorator(*args, **kwargs)
+        result = decorator(func, *args, **kwargs)
         return result
 
     return decorator_wrap
 
 
 @decorator_with_args_for_any_decorator
-def decorated_decorator(func: Callable, *args_decorator, **kwargs_decorator) -> Callable:
+def decorated_decorator(*args_decorator, **kwargs_decorator) -> Callable:
     def decorator(func: Callable):
         def wrap(*args, **kwargs):
             result = func(*args, **kwargs)
