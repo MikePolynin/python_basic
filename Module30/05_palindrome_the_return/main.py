9663bac3c6c4c88f9fc5cd095447667f92a60eb4
mike_p <mike.polynin@gmail.com>
2021-06-12 11:39:34 +0300

    30.05

--- Module30/05_palindrome_the_return/main.py
+++ Module30/05_palindrome_the_return/main.py
@@ -1,1 +1,27 @@
-# TODO здесь писать код
+import collections
+
+
+def can_be_poly(string: str) -> bool:
+    dict_1 = {}
+    dict_2 = {}
+    is_poly = True
+
+    for i in range(len(string)):
+        dict_1[i] = string[i]
+        dict_2[len(string) - 1 - i] = string[i]
+
+    ordered_dict_1 = collections.OrderedDict(dict_1)
+    ordered_dict_2 = collections.OrderedDict(dict_2)
+
+    for i in range(len(string)):
+        if ordered_dict_1.get(i) == ordered_dict_2.get(i):
+            continue
+        else:
+            is_poly = False
+
+    return is_poly
+
+
+if __name__ == '__main__':
+    print(can_be_poly('ababa'))
+    print(can_be_poly('abbbc'))
