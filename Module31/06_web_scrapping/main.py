ffdf923ec737a6c9b0df8fd7544b2bc604ffe7ea
mike_p <mike.polynin@gmail.com>
2021-06-23 00:48:40 +0300

    31

--- Module31/06_web_scrapping/main.py
+++ Module31/06_web_scrapping/main.py
@@ -4,12 +4,10 @@ import requests
 
 
 def web_scrapping():
-    h3_name_list = []
-
     my_req = requests.get('http://www.columbia.edu/~fdc/sample.html')
     data = my_req.text
 
-    h3_pattern = r'<h3.+>(.+)</h3>'
+    h3_pattern = r'<h3.+?>(.+?)</h3>'
 
     print(re.findall(h3_pattern, data))
 
